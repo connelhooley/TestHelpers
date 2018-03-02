d973488acf37454b0a6a7458730be79f2283cf51
Connel Hooley <connelhooley@gmail..com>
2018-03-02 14:05:05 +0000

    fixed csporj uncommented out a test

--- TestHelper/TestHelper.MediumTests/TestHelperInstanceTests/Generate.cs
+++ TestHelper/TestHelper.MediumTests/TestHelperInstanceTests/Generate.cs
@@ -1,38 +1,38 @@
-﻿//using System;
-//using ConnelHooley.TestHelper.MediumTests.CustomTypes;
-//using FluentAssertions;
-//using Xunit;
+﻿using System;
+using ConnelHooley.TestHelper.MediumTests.CustomTypes;
+using FluentAssertions;
+using Xunit;
 
-//namespace ConnelHooley.TestHelper.MediumTests.TestHelperInstanceTests
-//{
-//    public class Generate
-//    {
-//        [Fact]
-//        public void TestHelper_GenerateTypesFromConfigurators()
-//        {
-//            var sut = new TestHelperInstance(context => { });
+namespace ConnelHooley.TestHelper.MediumTests.TestHelperInstanceTests
+{
+    public class Generate
+    {
+        [Fact]
+        public void TestHelper_GenerateTypesFromConfigurators()
+        {
+            var sut = new TestHelperInstance(context => { });
 
-//            var result1 = sut.Generate<ExamplePoco1>();
-//            result1.WebSite.Should().Be(new Uri("http://example.com"));
+            var result1 = sut.Generate<ExamplePoco1>();
+            result1.WebSite.Should().Be(new Uri("http://example.com"));
 
-//            var result2 = sut.Generate<ExamplePoco2>();
-//            result2.Id.Should().Be(5);
-//        }
+            var result2 = sut.Generate<ExamplePoco2>();
+            result2.Id.Should().Be(5);
+        }
 
-//        [Fact]
-//        public void TestHelper_GenerateTypesInstanceConfig()
-//        {
-//            var sut = new TestHelperInstance(context =>
-//            {
-//                context.Register(() => new ExamplePoco1
-//                {
-//                    Name = context.Generate<string>(),
-//                    WebSite = new Uri("http://example2.co.uk")
-//                });
-//            });
+        [Fact]
+        public void TestHelper_GenerateTypesInstanceConfig()
+        {
+            var sut = new TestHelperInstance(context =>
+            {
+                context.Register(() => new ExamplePoco1
+                {
+                    Name = context.Generate<string>(),
+                    WebSite = new Uri("http://example2.co.uk")
+                });
+            });
 
-//            var result = sut.Generate<ExamplePoco1>();
-//            result.WebSite.Should().Be(new Uri("http://example2.co.uk"));
-//        }
-//    }
-//}
+            var result = sut.Generate<ExamplePoco1>();
+            result.WebSite.Should().Be(new Uri("http://example2.co.uk"));
+        }
+    }
+}

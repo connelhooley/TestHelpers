6c34b790a841cd82544b528ffa02b583a353ebf2
Connel Hooley <connelhooley@gmail..com>
2018-03-02 19:30:53 +0000

    Configurators solution

---
+++ TestHelper.Configurators/TestHelper.Configurators.Akka/AkkaTestHelperConfigurator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Akka.Actor;
+using ConnelHooley.TestHelper.Abstractions;
+
+namespace ConnelHooley.TestHelper.Configurators.Akka
+{
+    public class AkkaTestHelperConfigurator : ITestHelperConfigurator
+    {
+        public void Configure(ITestHelperContext x)
+        {
+            x.Register(() => ActorPath.Parse($"akka://user/{Guid.NewGuid()}"));
+        }
+    }
+}

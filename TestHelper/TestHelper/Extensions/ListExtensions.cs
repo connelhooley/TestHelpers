0fd4b4dacf6efcb9dc278bfb751c882b4e8f808d
Connel Hooley <connelhooley@gmail..com>
2018-03-02 16:17:33 +0000

    Fixed extension method Added README to solution

--- TestHelper/TestHelper/Extensions/ListExtensions.cs
+++ TestHelper/TestHelper/Extensions/ListExtensions.cs
@@ -4,7 +4,7 @@ namespace ConnelHooley.TestHelper.Extensions
 {
     public static class ListExtensions
     {
-        public static T ChooseRandomItem<T>(this List<T> @this) =>
-            TestHelper.ChooseRandomItem(@this);
+        public static T TakeRandomItem<T>(this List<T> @this) =>
+            TestHelper.TakeRandomItem(@this);
     }
 }
